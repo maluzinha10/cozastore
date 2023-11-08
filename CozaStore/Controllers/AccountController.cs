afc25a525e72526f74e053d631508a1460d134dc
Maria Luiza <marialuizagon1008@gmail.com>
2023-11-08 12:19:13 -0300

    Finalizado o Login e começo do resister

--- CozaStore/Controllers/AccountController.cs
+++ CozaStore/Controllers/AccountController.cs
@@ -3,87 +3,108 @@ using CozaStore.ViewModels.Account;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
+using System.Security.Claims;
 
 namespace CozaStore.Controllers;
 
-    public class AccountController : Controller
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppDbContext _contexto;
+    public AccountController(
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+         UserManager<IdentityUser> userManager,
+         AppDbContext contexto)
+
     {
-        private readonly ILogger<AccountController> _logger;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly AppDbContext _contexto;
-        public AccountController(
-            ILogger<AccountController> logger,
-            SignInManager<IdentityUser> signInManager,
-             UserManager<IdentityUser> userManager,
-             AppDbContext contexto)
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _contexto = contexto;
+    }
 
+    [HttpGet]
+
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM login = new()
         {
-            _logger = logger;
-            _signInManager = signInManager;
-             _userManager = userManager;
-             _contexto = contexto;
-        }
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(login);
+    }
 
-        [HttpGet]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
 
-        public IActionResult Login(string returnUrl)
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
         {
-            LoginVM login = new() {
-              UrlRetorno = returnUrl ?? Url.Content("~/")  
-            };
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
-        public async Task<IActionResult> Login(LoginVM login)
-        {
-            if (ModelState.IsValid)
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
             {
-                string userName = login.Email;
-                if (IsValidEmail(login.Email))
-                {
-                    var user = await _userManager.FindByEmailAsync(login.Email);
-                    if (user != null)
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
                     userName = user.UserName;
-                }
-                var result = await _signInManager.PasswordSignInAsync(
-                    userName, login.Senha, login.Lembrar, lockoutOnFailure: true
-                );
-
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
-                    return LocalRedirect(login.UrlRetorno);
-                }
-                
-                if (result.Succeeded)
-                {
-                    _logger.LogWarning($"Usuário {login.Email} está bloqueado");
-                    return RedirectToAction("Lockout");
-                }
-                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
-                
             }
-            return View(login);
-        }
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
 
-        public static bool IsValidEmail(string email)
-        {
-            try{
-                MailAddress mail = new(email);
-                return true;
-            } catch {
-                return false;
-
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário {login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
             }
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+            if (result.Succeeded)
+            {
+                _logger.LogWarning($"Usuário {login.Email} está bloqueado");
+                return RedirectToAction("Lockout");
+            }
+            ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos!!!");
+
+        }
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        RegisterVM register = new();
+        return View(register);
+    }
+    public static bool IsValidEmail(string email)
+    {
+        try
         {
-            return View("Error!");
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+
         }
     }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View("Error!");
+    }
+}
